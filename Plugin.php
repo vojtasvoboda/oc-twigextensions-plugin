7373dd0d2ff33842c733ba89353571f2a333eb43
Sebastian Hilger <hi@7talents.de>
2016-04-18 12:51:47 +0200

    Added config() functionality

--- Plugin.php
+++ Plugin.php
@@ -54,6 +54,9 @@ class Plugin extends PluginBase
         // add String Loader functions
         $functions += $this->getStringLoaderFunctions($twig);
 
+        // add Config function
+        $functions += $this->getConfig();
+
         // add Text extensions
         $filters += $this->getTextFilters($twig);
 
@@ -233,6 +236,20 @@ class Plugin extends PluginBase
             },
             'rightpad' => function($string, $pad_length, $pad_string = ' ') {
                 return str_pad($string, $pad_length, $pad_string, $pad_type = STR_PAD_RIGHT);
+            },
+        ];
+    }
+
+    /**
+     * Works like the config() function
+     *
+     * @return array
+     */
+    private function getConfig()
+    {
+        return [
+            'config' => function($key) {
+                return config($key);
             },
         ];
     }
