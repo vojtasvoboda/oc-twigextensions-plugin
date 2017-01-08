1964efa0576a4b333f5260630eaea253682d294a
Ronaldo Ribeiro de Sousa <ribbsousa@gmail.com>
2017-01-08 14:40:59 -0300

    Create lang.php
    Add language pt-br

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'diff' => [
+        'empty' => 'agora',
+        'ago' => [
+            'year' => '1 ano atrás|:count anos atrás',
+            'month' => '1 mês atrás|:count mêses atrás',
+            'day' => '1 dia atrás|:count dias atrás',
+            'hour' => '1 hora atrás|:count horas atrás',
+            'minute' => '1 minuto atrás|:count minutos atrás',
+            'second' => '1 segundo atrás|:count segundos atrás',
+        ],
+        'in' => [
+            'second' => 'em 1 segundo|em :count segundos',
+            'hour' => 'em 1 hora|em :count horas',
+            'minute' => 'em 1 minuto|em :count minutos',
+            'day' => 'em 1 dia|em :count dias',
+            'month' => 'em 1 mês|em :count mêses',
+            'year' => 'em 1 ano|em :count anos',
+        ],
+    ],
+];
