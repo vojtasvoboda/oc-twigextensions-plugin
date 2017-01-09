4284c3014e379233a016068fe44dfd81ba0289a3
Sebastian Hilger <hi@7talents.de>
2017-01-09 09:07:05 +0100

    German time diff translations

---
+++ lang/de/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'diff' => [
+        'empty' => 'jetzt',
+        'ago' => [
+            'year' => '1 year ago|vor :count Jahren',
+            'month' => '1 month ago|vor :count Monaten',
+            'day' => '1 day ago|vor :count Tagen',
+            'hour' => '1 hour ago|vor :count Stunden',
+            'minute' => '1 minute ago|vor :count Minuten',
+            'second' => '1 second ago|vor :count Sekunden',
+        ],
+        'in' => [
+            'second' => 'in 1 second|in :count Sekunden',
+            'hour' => 'in 1 hour|in :count Stunden',
+            'minute' => 'in 1 minute|in :count Minuten',
+            'day' => 'in 1 day|in :count Tagen',
+            'month' => 'in 1 month|in :count Monaten',
+            'year' => 'in 1 year|in :count Jahren',
+        ],
+    ],
+];
