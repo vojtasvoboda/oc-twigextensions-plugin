d38eb5aa693728f6981888a482365b27fd99bcac
Marko Kodrič <marko.kodric@spletna-postaja.com>
2020-10-01 12:19:01 +0200

    Added slovenian translations

---
+++ lang/sl/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'diff' => [
+        'empty' => 'zdaj',
+        'ago' => [
+            'year' => 'pred 1 letom|pred :count leti',
+            'month' => 'pred 1 mesecem|pred :count meseci',
+            'day' => 'pred 1 dnevom|pred :count dnevi',
+            'hour' => 'pred 1 uro|pred :count urami',
+            'minute' => 'pred 1 minuto|pred :count minutami',
+            'second' => 'pred 1 sekundo|pred :count sekundami',
+        ],
+        'in' => [
+            'second' => 'v 1 sekundi|v :count sekundah',
+            'hour' => 'v 1 uri|v :count urah',
+            'minute' => 'v 1 minuti|v :count minutah',
+            'day' => 'v 1 dnevu|v :count dneh',
+            'month' => 'v 1 mesecu|v :count mesecih',
+            'year' => 'v 1 letu|v :count letih',
+        ],
+    ],
+];
