658de01f52d2d386fc1f8961d3c5d11c1f5079bb
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2025-08-10 09:04:46 +0200

    Remove unused require_once

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -6,7 +6,7 @@ use Config;
 use PluginTestCase;
 use Twig_Environment;
 
-require_once __DIR__.'/../vendor/autoload.php';
+// require_once __DIR__.'/../vendor/autoload.php';
 
 class PluginTest extends PluginTestCase
 {
